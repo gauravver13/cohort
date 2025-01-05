a59bb740ce56c45d329daaff17d4b2bda4755805
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-01-06 04:19:32 +0530

    react revised

--- week9/todo/src/App.jsx
+++ week9/todo/src/App.jsx
@@ -1,32 +1,84 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react"
 
 function App() {
-  const [todos, setTodos] = useState({
-    title: "",
-    description: "",
-    done: fasle
-  })
+  const [counterVisible, setCounterVisible] = useState(true);
 
-  function addTodo() {
-    let newArray = []
-    for (let i=0; i<todos.length; i++) {
-      newArray.push(todos[i])
-    }
+  // useEffect(function(){
+  //   setInterval(function(){
+  //     setCounterVisible(c => !c);
+  //   }, 5000)
+  // },[])
 
-    newArray.push({
-      title: "Hey",
-      description: "",
-      done: true
-    });
-    setTodos(newArray);
+  return <div>
+
+    Hi there
+    <br />
+    {counterVisible?<Counter></Counter>: null}
+    {counterVisible && <Counter></Counter>}
+    
+  </div>
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+
+  console.log('counted');
+
+
+  
+
+  // useEffect(function(){
+  //   let clock = setInterval(function increaseCount() {
+  //     setCount(c => c+1);
+  //     console.log('inside interval!');
+      
+  //     // setCount(function(count) {
+  //     //   return count+1;
+  //     // });
+  //   }, 1000)
+  //   console.log('Mounted');
+
+  //   return function(){
+  //     clearInterval(clock)
+  //     console.log('Unmounted');
+  //   }
+  // }, [])
+
+  // setInterval(  function increaseCount() {
+  //   setCount(count+1);
+  // }, 1000)
+
+  // useEffect(function(props){
+  //     setInterval(function(){
+  //       setCount(c => c+1)
+  //     })
+  //     return 
+  // }, [])
+
+  function increaseCount() {
+    setCount(count+1);
+  }
+  function decreaseCount() {
+    setCount1(count1-1);
+  }
+  function resetCount() {
+    setCount(0);
   }
 
-  return (
-    <div>
-      <button onClick={addTodo}>Add Todo</button>
-    </div>
-  )
+  return <div>
+    <h1>{count}</h1>
+    <h1>{count1}</h1>
+    <button onClick={increaseCount}>Increase Count</button>
+    <button onClick={decreaseCount}>Descrease Count</button>
+    <button onClick={resetCount}>Reset Count</button>
+  </div>
 }
 
 export default App
+
+
+// Raw manupulation can be done, But react takes all the hard part and makes our life simpler.
+
+// conditional rendering, Mounted, UnMounted, CleanUp, dependency array!
+// Hooks(useState, useEffect)
