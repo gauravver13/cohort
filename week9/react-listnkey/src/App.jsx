0d183f63157dc2c04d45f02f80db4eb90b881cb8
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-01-08 23:33:20 +0530

    commit react list

---
+++ week9/react-listnkey/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+
+
+function App() {
+
+  const todos = [{
+    title: "Go to gym and gift yourself a healthy and active body!",
+    done: false
+  }, {
+    title: "Complete cohort in jan only",
+    done: false
+  }, {
+    title: "Research and get your hands dirty on the ongoing startup.",
+    done: false
+  }, {
+    title: "You are good to go for your travel agency startup",
+    done: false
+  }, {
+    title: "Make a platform like Bla-Bla",
+    done: false
+  }, {
+    title: "Learn Web3 in 2025",
+    done: false
+  }, {
+    title: "Finish all of the above in 2025 only",
+    done: false
+  },
+]
+
+const todoComponents = todos.map( todo => <Todo
+  title={todo.title} done={todo.done} />)
+
+  return (
+    <>
+        {todoComponents}
+    </>
+  )
+}
+
+function Todo( {title, done} ) {
+  return <div>
+    <h4>{title}</h4> - {done ? "Done!": "Not Done"} 
+    
+  </div>
+}
+
+export default App
