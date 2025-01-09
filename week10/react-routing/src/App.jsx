3b388571ac6cf02d54a90ae43ed77afed841bad6
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-01-10 00:28:42 +0530

    add layout and routing path!

--- week10/react-routing/src/App.jsx
+++ week10/react-routing/src/App.jsx
@@ -1,8 +1,26 @@
 
-import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";    //Hash-Router(Chrome extension)
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";    //Hash-Router(Chrome extension)
 
 
 function App() {
+
+
+
+// JUST ANOTHER WAY TO REACT:: 
+  // const router = [
+  //   {
+  //     path: "/fgh/cvbn.fghjk",
+  //     element: <Landing />
+  //   },
+  //   {
+  //     path: "/fgh/cvbn.fghjk",
+  //     element: <fghj />
+  //   },
+  //   {
+  //     path: "/fgh/cvbn.fghjk",
+  //     element: <dfghj />
+  //   },
+  // ]
 
   return <>
 
@@ -14,23 +32,26 @@ function App() {
   </h2> */}
 
       <BrowserRouter>
-        <h3>Single Page Applications: SPA's</h3>
-        <h2>
+        {/* <h3>Single Page Applications: SPA's</h3> */}
+        {/* <h2>
+        HEADER
           <Link to="/">ALLEN </Link> 
           |
           <Link to="/neet/online-coaching-class-11">CLASS-11 </Link>
           |
           <Link to="/neet/online-coaching-class-12">CLASS-12 </Link> 
           |
-        </h2>
+        </h2> */}
         <Routes>
-          <Route path="/neet/online-coaching-class-11" element={<Class11Program />}/>
-          <Route path="/neet/online-coaching-class-12" element={<Class12Program />}/>
-          <Route path="/" element={<Landing />}/>
-          <Route path="*" element={<ErrorPage />}/>
+          <Route path="/neet" element={<Layout />} >
+            <Route path="/neet/online-coaching-class-11" element={<Class11Program />}/>
+            <Route path="/neet/online-coaching-class-12" element={<Class12Program />}/>
+          </Route>
+            <Route path="/" element={<Landing />}/>
+            <Route path="*" element={<ErrorPage />}/>
         </Routes>
       </BrowserRouter>
-      <div>Hi there</div>
+      {/* <div>FOOTER | Contact Us</div> */}
   </>
 }
 
@@ -55,6 +76,37 @@ function Class12Program() {
 function Landing(){
   return <div>
     Welcome to allen!
+    <h1>Hello </h1>
+  </div>
+}
+
+function Layout() {
+  return <div>
+    <Header />
+    <div style={{height:"70vh", backgroundColor: "green"}}>
+      <Outlet />
+    </div>
+    <Footer />
+  </div>
+}
+
+function Header() {
+  return <div style={{height: "10vh", backgroundColor: "red"}}>
+          <h2 style={{borderRadius: '100px'}}>Header</h2>
+            <div>
+              <Link to="/">ALLEN </Link> 
+              |
+              <Link to="/neet/online-coaching-class-11">CLASS-11 </Link>
+              |
+              <Link to="/neet/online-coaching-class-12">CLASS-12 </Link> 
+            </div>
+      </div>
+}
+
+function Footer() {
+  return <div style={{backgroundColor: 'pink', height: "40vh"}}>
+        Footer...
+        <div>FOOTER | Contact Us</div>
   </div>
 }
 
