e317c0dfe5ca54103f5761fbc6bc2dcf01d51534
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-02-25 01:49:28 +0530

    typescript assignments

--- week14/index.ts
+++ week14/index.ts
@@ -10,3 +10,60 @@ let x: number = 1;      // type-inferencing
 // x = "harkirat boss";         // error
 
 console.log(x);
+
+let person: string = "Gaurav";
+
+let user: string = `Hello ${person}`;
+
+
+console.log('USER:', user);
+
+function greet(firstName: string) {
+    console.log(`Hello ${firstName}`);
+}
+
+
+greet("Gaurav");
+greet("Harkirat");
+// greet(1);      // error
+greet("1");
+
+
+
+let y: any = 1;
+
+
+y = true;
+y = "harkirat";
+y = [1, 2, 3, 4, 5];
+
+// number, string, boolean, null, undefined, any, void, never, object, array, tuple, enum, union, literal, ----> type-assertion
+
+
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+
+console.log(sum(1, 2));
+
+
+function isLegal(age: number): boolean {
+    return age >= 18;
+}
+
+console.log(isLegal(18));
+console.log(isLegal(17));
+
+
+function delayedCall(callbackFn: () => void) {
+    setTimeout(callbackFn, 2000);
+}
+
+
+console.log("Before delayed call");
+delayedCall(() => {
+    console.log("Inside delayed call");
+});
+console.log("After delayed call before 2 sec");
+
