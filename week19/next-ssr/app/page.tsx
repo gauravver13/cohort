3fbaec528d4c9031a25f855a7bd5851e6709ca8c
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-05 02:59:14 +0530

    file based rendering shown in loader!

--- week19/next-ssr/app/page.tsx
+++ week19/next-ssr/app/page.tsx
@@ -1,22 +1,39 @@
-import axios from 'axios';
+'use client'
 
-export default async function Home() {
-  const loading = true;
-  const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-  console.log(`Request went out!!`);
-  const data = response.data;
 
-  if(loading) {
-    return <h2>Loading...</h2>
-  }
+export default function User() {
+const [loading, setLoading] = useState(true)
+const [data, setData] = useState<{id: number; title: string; completed: boolean; } | null>(null)
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos/2')
+    .then((response) => {
+      console.log(`Request went out!!`)
+      setData(response.data)
+      setLoading(false);
+    })
+  }, [])
 
   return (
     <div>
-      <h1>Home</h1>
-      <h2>{data.id}</h2>
-      <h2>{data.name}</h2>
-      <h2>{data.email}</h2>
+      <h1>User</h1>
+      {loading ? <h2>Loading...</h2> : (
+        <>
+          <h2>{data?.id}</h2>
+          <h2>{data?.title}</h2>
+          <h2>{data?.completed}</h2>
+        </>
+      )}
     </div>
-  );
+  )
 }
+
+
+// this is a  bad way to fetch data in next.js,
+// because we should be taken care of the server side rendering and SEO optimization. which is not possible with this approach.
+
+// Atleast for the landing page we should be taken care of the SEO optimization and server side rendering.
+// go through the home page for this approach.
--- week19/next-ssr/app/user/page.tsx
+++ week19/next-ssr/app/user/page.tsx
@@ -1,39 +1,24 @@
-'use client'
+import axios from 'axios';
 
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+export default async function User() {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+
+  console.log(`Request went out!!`);
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
 
-export default function User() {
-const [loading, setLoading] = useState(true)
-const [data, setData] = useState<{id: number; title: string; completed: boolean; } | null>(null)
+  const data = response.data;
 
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/2')
-    .then((response) => {
-      console.log(`Request went out!!`)
-      setData(response.data)
-      setLoading(false);
-    })
-  }, [])
+
+
 
   return (
     <div>
-      <h1>User</h1>
-      {loading ? <h2>Loading...</h2> : (
-        <>
-          <h2>{data?.id}</h2>
-          <h2>{data?.title}</h2>
-          <h2>{data?.completed}</h2>
-        </>
-      )}
+      <h1>Home</h1>
+      <h2>{data.id}</h2>
+      <h2>{data.name}</h2>
+      <h2>{data.email}</h2>
     </div>
-  )
+  );
 }
-
-
-// this is a  bad way to fetch data in next.js,
-// because we should be taken care of the server side rendering and SEO optimization. which is not possible with this approach.
-
-// Atleast for the landing page we should be taken care of the SEO optimization and server side rendering.
-// go through the home page for this approach.
