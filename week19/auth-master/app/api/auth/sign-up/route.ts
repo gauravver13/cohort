26a06c15c6bd21a788eb0cb48dd5c43933d6a579
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-05 15:30:08 +0530

    connect backend and auth-frontend in same nextjs server

--- week19/auth-master/app/api/auth/sign-up/route.ts
+++ week19/auth-master/app/api/auth/sign-up/route.ts
@@ -1,7 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import { PrismaClient } from "@prisma/client";
+
+
+const prismaClient = new PrismaClient();
+
 export async function POST(req: NextRequest) {
   const body = await req.json();
+
+  prismaClient.user.client.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
   console.log("Request body:", body);
   return NextResponse.json({ message: "Sign-in successful" });
 }
