7165de55e1cb6782167b98c2c61eb7a72f3dc667
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-07 03:53:32 +0530

    add endpoints and middlewares

--- week15/server/src/index.ts
+++ week15/server/src/index.ts
@@ -1,10 +1,42 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import cors from 'cors';
+import { userMiddleware } from './middleware';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+//EndPoints:
+
+app.post('/api/v1/signup', async (req, res) => {
+    
+})
+
+app.post('/api/v1/signin', async (req, res) => {
+
+})
+
+app.post('/api/v1/content', userMiddleware, async (req, res) => {
+
+})
+
+app.get('/api/v1/content', userMiddleware, async (req, res) => {
+
+})
+
+app.delete('/api/v1/content', userMiddleware, async (req, res) => {
+
+})
+
+app.post('/api/v1/brain/share', userMiddleware, async (req, res) => {
+
+})
+
+
+app.get('/api/v1/brain/:shareLink', async (req, res) => {
+
+})
 
 
 app.listen(3000, () => {
---
+++ week15/server/src/middleware.ts
@@ -0,0 +1,4 @@
+import { NextFunction, Request, Response } from "express";
+
+export const userMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+}
