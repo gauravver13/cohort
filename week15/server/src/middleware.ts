30453e5250cbc9b63986557e5ecbf54aa41592ec
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-10 02:57:59 +0530

    add middleware

--- week15/server/src/middleware.ts
+++ week15/server/src/middleware.ts
@@ -1,4 +1,30 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken'
+import dotenv from "dotenv";
+import { JWT_SECRET } from "./config";
 
-export const userMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+
+// @ts-ignore
+export const userMiddleware = async (req, res, next) => {
+    try {
+        const token = req.headers.token?.split(' ')[1];         // extract bearer token
+
+        if(!token) {
+            return res.status(401).json({
+                message: 'Access denied, No token provided'
+            })
+        
+        const decodedToken = jwt.verify(token, JWT_SECRET);
+
+        req.user = decodedToken;
+
+        next();
+
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Invalid or expired token'
+        })
+    }
 }
+ 
