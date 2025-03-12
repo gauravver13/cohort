5cfa0a64c00203a10ba0357ad415fc6af3333203
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-12 22:00:12 +0530

    add next-auth via credentials

---
+++ week20/next-auth/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,43 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Login with email",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+              username: { label: "Username", type: "text", placeholder: "igaurav@gmail.com" },
+              password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const username = credentials?.username;
+                const password = credentials?.password;
+
+                console.log(username)
+                console.log(password)
+                const user = {
+                    id: '1',
+                    name: 'gaurav',
+                    username: "igaurav"
+                }
+
+                // db request to check if this username and password are correct!
+
+                // set the db here
+                if(user) {
+                    return user
+                } else {
+                    return null;
+                }
+            }
+          })
+    ]
+})
+
+
+export { handler as GET, handler as POST }
