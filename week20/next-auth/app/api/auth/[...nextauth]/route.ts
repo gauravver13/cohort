d04586bf1cd62772474f3f0dee4c4783f1bf5f0c
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-12 22:44:50 +0530

    add next-auth provides: googleProvider and githbProvider

--- week20/next-auth/app/api/auth/[...nextauth]/route.ts
+++ week20/next-auth/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+
 
 const handler = NextAuth({
     providers: [
         CredentialsProvider({
             // The name to display on the sign in form (e.g. "Sign in with...")
-            name: "Login with email",
+            // name: "Login with email",
+            name: "email",
             // `credentials` is used to generate a form on the sign in page.
             // You can specify which fields should be submitted, by adding keys to the `credentials` object.
             // e.g. domain, username, password, 2FA token, etc.
@@ -35,7 +40,21 @@ const handler = NextAuth({
                     return null;
                 }
             }
+          }),
+
+        GoogleProvider({
+            // clientId: process.env.GOOGLE_CLIENT_ID,
+            // clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            clientId: "asd",
+            clientSecret: "zxcv"
+          }),
+
+          GitHubProvider({
+            clientId: "zxc",
+            clientSecret: "uiop"
           })
+
+        
     ]
 })
 
