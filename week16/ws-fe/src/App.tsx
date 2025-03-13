f66bfacc76bccb3d65d5b9cab015d7808b9c636a
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-14 03:43:37 +0530

    add webSocket frontend to its backedn

--- week16/ws-fe/src/App.tsx
+++ week16/ws-fe/src/App.tsx
@@ -1,9 +1,55 @@
+"use client";
+import { useEffect, useState } from "react"
+
+// More elegant useSocket hook!!
 export default function App() {
+  const [socket, setSocket] = useState();
+
+  function sendMessage(){
+    if(!socket) {
+      return;
+    }
+
+    // @ts-ignore
+    socket.send("ping");
+  }
+
+  // This will render only when the components mount or dependencies changes-means once()! otherwise bad approach- fo=figure out more about this. 
+  useEffect(() => {
+    // fetch("ws://localhost:8080/users")
+    const ws = new WebSocket("ws://localhost:8080");
+    // @ts-ignore
+    setSocket(ws);
+
+    ws.onmessage = (ev) => {
+      alert(ev.data);
+    }
+
+    // ws.onerror = () => {
+
+    // }
+
+    // ws.close = () => {
+      
+    // }
+
+    // ws.onopen = () => {
+
+    // }
+
+
+
+  }, [])
   return (
     <>
-    <h1 className="bg-red-300">Hello World</h1>
-    <h1>hello</h1>
-    <div>Hi There</div>
+      <h1 className="text-center mb-2">CHAT APPLICATION</h1>
+      <div className="bg-red-300 rounded-md">
+        <label htmlFor="" className="text-black font-bold">Message: </label>
+      <input type="text" name="" placeholder="message" className="bg-white text-black rounded-lg"></input>
+      <button className="font-semibold m-2 p-auto rounded-md bg-red-600"
+      onClick={sendMessage}>Send</button>
+      </div>
+
     </>
   )
 }
