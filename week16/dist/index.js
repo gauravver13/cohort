d431cf90b2657958b2d59f903bb68bba8cf96070
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-13 19:09:13 +0530

    fix wss connection

--- week16/dist/index.js
+++ week16/dist/index.js
@@ -4,13 +4,20 @@ const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 // event handler:
 wss.on("connection", function (socket) {
-    console.log('User connected');
-    socket.send('Hi there!');
-    setInterval(() => {
-        socket.send('Current price of Solana is:: ' + Math.random());
-    }, 3000);
+    // console.log('User connected');
+    // socket.send('Hi there!');
+    // setInterval(() => {
+    //     socket.send('Current price of Solana is:: '+ Math.random());
+    // }, 3000)
+    // socket.on("message", (e) => {
+    //     console.log('EVENT: ', e);
+    //     console.log('EVENT: ', e.toString());
+    // })
     socket.on("message", (e) => {
-        console.log('EVENT: ', e);
-        console.log('EVENT: ', e.toString());
+        console.log('USer send ping');
+        if (e.toString() === "ping") {
+            socket.send("pong");
+            // socket.emit("pong"); 
+        }
     });
 });
--- week16/src/index.ts
+++ week16/src/index.ts
@@ -5,15 +5,23 @@ const wss = new WebSocketServer({ port: 8080 });
 
 // event handler:
 wss.on("connection", function(socket) {
-    console.log('User connected');
-    socket.send('Hi there!');
+    // console.log('User connected');
+    // socket.send('Hi there!');
 
-    setInterval(() => {
-        socket.send('Current price of Solana is:: '+ Math.random());
-    }, 3000)
+    // setInterval(() => {
+    //     socket.send('Current price of Solana is:: '+ Math.random());
+    // }, 3000)
+
+    // socket.on("message", (e) => {
+    //     console.log('EVENT: ', e);
+    //     console.log('EVENT: ', e.toString());
+    // })
 
     socket.on("message", (e) => {
-        console.log('EVENT: ', e);
-        console.log('EVENT: ', e.toString());
+        console.log('USer send ping')
+        if(e.toString() === "ping") {
+            socket.send("pong");
+            // socket.emit("pong"); 
+        }
     })
 })
