8eb595a41c71028caf660ab25d53b9a209fe18a7
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-03-15 01:58:59 +0530

    add fresh ws server connects backend and react-fe

--- week16/ws-fe/src/App.tsx
+++ week16/ws-fe/src/App.tsx
@@ -1,51 +1,98 @@
-"use client";
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 // More elegant useSocket hook!!
 export default function App() {
-  const [socket, setSocket] = useState();
+  // const [socket, setSocket] = useState();
+  // const inputRef = useRef("");
 
-  function sendMessage(){
-    if(!socket) {
+  // function sendMessage(){
+  //   if(!socket) {
+  //     return;
+  //   }
+    
+  //   // @ts-ignore
+  //   const message = inputRef.current.value;
+  //   console.log('socket message sending')
+  //   // @ts-ignore
+  //   socket.send(message)
+  //   console.log('socket sent')
+
+
+  //   // hard-coded stuffs: 
+  //   // // @ts-ignore
+  //   // socket.send("ping");
+  // }
+
+  // // This will render only when the components mount or dependencies changes-means once()! otherwise bad approach- fo=figure out more about this. 
+  // useEffect(() => {
+  //   // fetch("ws://localhost:8080/users")
+  //   const ws = new WebSocket("ws://localhost:8080");
+    
+  //   console.log('socket is going to hung in fe');
+  //   // @ts-ignore
+  //   setSocket(ws);
+
+  //   console.log('Set-Socket is hungged in fe');
+
+  //   ws.onmessage = (ev) => {
+  //     alert(ev.data);
+  //     console.log('IS this called')
+  //   }
+
+  //   console.log("ws boomed");
+
+  //   // ws.onerror = () => {
+
+  //   // }
+
+  //   // ws.close = () => {
+      
+  //   // }
+
+  //   // ws.onopen = () => {
+
+  //   // }
+  // }, [])
+
+
+  // NEW SCENE BUT SAME:
+
+  const [ws, setWs] = useState("");
+  const inputRef = useRef(""); 
+
+  function sendMessage() {
+    if(!ws) {
       return;
     }
 
     // @ts-ignore
-    socket.send("ping");
+    const message = inputRef.current.value;
+
+    // @ts-ignore
+    ws.send(message);
   }
 
-  // This will render only when the components mount or dependencies changes-means once()! otherwise bad approach- fo=figure out more about this. 
   useEffect(() => {
-    // fetch("ws://localhost:8080/users")
     const ws = new WebSocket("ws://localhost:8080");
-    // @ts-ignore
-    setSocket(ws);
 
-    ws.onmessage = (ev) => {
-      alert(ev.data);
+    // @ts-ignore
+    setWs(ws);
+
+    ws.onmessage = (e) => {
+      console.log(e.data)
+      alert(e.data);
     }
 
-    // ws.onerror = () => {
-
-    // }
-
-    // ws.close = () => {
-      
-    // }
-
-    // ws.onopen = () => {
-
-    // }
-
-
-
   }, [])
+
+
+
   return (
     <>
       <h1 className="text-center mb-2">CHAT APPLICATION</h1>
       <div className="bg-red-300 rounded-md">
         <label htmlFor="" className="text-black font-bold">Message: </label>
-      <input type="text" name="" placeholder="message" className="bg-white text-black rounded-lg"></input>
+      <input ref={inputRef} type="text" name="" placeholder="message" className="bg-white text-black rounded-lg"></input>
       <button className="font-semibold m-2 p-auto rounded-md bg-red-600"
       onClick={sendMessage}>Send</button>
       </div>
